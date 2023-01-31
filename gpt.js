4eac558692eeb09bce2879e97d8748a5ed9c3039
ashishatwork <ashishkumariitr.0@gmail.com>
2023-02-01 03:03:57 +0530

    ui modified

--- gpt.js
+++ gpt.js
@@ -1,8 +1,17 @@
 document.getElementById("chat").addEventListener("click", submitForm);
+
+var wage = document.getElementById("chat");
+wage.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        submitForm();
+    }
+});
+
 function submitForm() {
+    console.log("SUBMITTED!!")
     var name = document.getElementById("name").value;
-    console.log(name)
-    fetch('https://nqui5y.deta.dev/text', {
+    console.log("name")
+    fetch('https://dbarf8.deta.dev/text', {
         method: "POST",
         body: JSON.stringify({ prompt: name }),
         headers: { 'Content-Type': 'application/json' }
@@ -10,5 +19,6 @@ function submitForm() {
       .then(data => {
           console.log(data);
           document.getElementById("response").innerHTML = data.message;
-      })
+     })
+    //document.getElementById("response").innerHTML ="lorem ipsum"
 }
